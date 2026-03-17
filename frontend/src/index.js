7f4c16e60f7eb2056ae32c8e46275d64648e0811
Binil jacob <biniljacob274@gmail.com>
2026-03-17 14:25:53 +0530

    Remove global LoadScript from index.js to fix loading maps flash on /

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { LoadScript } from '@react-google-maps/api';
 import App from './App';
-
-// Libraries must be defined outside component or memoized to prevent re-initialization
-const LIBRARIES = ['places', 'geometry'];
-const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || '';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoadScript 
-      googleMapsApiKey={googleMapsApiKey}
-      libraries={LIBRARIES}
-      loadingElement={<div>Loading Maps...</div>}
-    >
-      <App />
-    </LoadScript>
+    <App />
   </React.StrictMode>
 );
